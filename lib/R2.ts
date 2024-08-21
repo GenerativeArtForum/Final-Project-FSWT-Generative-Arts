d270208b1c97946f57fc0608fcdf8f25de08ef10
sherzock <jacobo.galofre@gmail.com>
2024-08-21 19:51:48 +0200

    Created R2 class for easier export with a upload file function

--- lib/R2.ts
+++ lib/R2.ts
@@ -3,26 +3,46 @@ import { extname } from "path";
 import { env } from "@/lib/env";
 import { readFile } from "fs/promises";
 
-
-const bucket = env("R2_BUCKET");
-
-const client = new S3Client({
-    region: env("R2_REGION"),
-    credentials: {
-        accessKeyId: env("R2_ACCESS_KEY"),
-        secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
-    },
-    endpoint: env("R2_ENDPOINT"),
-});
-
-const command = new PutObjectCommand({
-    Bucket: bucket,
-    Key: "test",
-    Body: await readFile("bun.lockb"),
-    ACL: "public-read",
-    ContentType: "application/octet-stream",
-});
-
-await client.send(command);
-
-client.destroy();
+const mimeTypes: Record<string, string> = {
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+  };
+  class R2Client {
+    s3client: S3Client;
+    bucket: string;
+  
+    constructor({ bucket }: { bucket: string }) {
+      this.s3client = new S3Client({
+        region: env("R2_REGION"),
+        credentials: {
+          accessKeyId: env("R2_ACCESS_KEY"),
+          secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+        },
+        endpoint: env("R2_ENDPOINT"),
+      });
+      this.bucket = bucket;
+    }
+  
+    async uploadFile(filename: string, buffer: Buffer) {
+      const extension = extname(filename);
+      await this.s3client.send(
+        new PutObjectCommand({
+          Bucket: this.bucket,
+          Key: filename,
+          Body: buffer,
+          ACL: "public-read",
+          ContentType: mimeTypes[extension] || "application/octet-stream",
+        })
+      );
+    }
+  
+    destroy() {
+      this.s3client.destroy();
+    }
+  }
+  
+  //Generates the R2Client
+  export const r2client = new R2Client({
+    bucket: env("R2_BUCKET"),
+  });
