26b00f562686e231897dac9d5dc6c9beb863b15d
sherzock <jacobo.galofre@gmail.com>
2024-09-11 19:04:59 +0200

    Rate limiting implemented, user can only upload 5 images per hour

--- actions/upload-image.ts
+++ actions/upload-image.ts
@@ -4,15 +4,42 @@ import { r2client } from "@/lib/R2";
 import { extname } from "path";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 
-
-function buf2hex(buffer: ArrayBuffer) { // buffer is an ArrayBuffer
+function buf2hex(buffer: ArrayBuffer) {
+  // buffer is an ArrayBuffer
   return [...new Uint8Array(buffer)]
-      .map(x => x.toString(16).padStart(2, '0'))
-      .join('');
+    .map((x) => x.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+//SetUp de Rate Limiting
+const userUploads = new Map<string, { count: number; lastReset: number }>();
+const RATE_LIMIT = 5; // 5 uploads por hora
+const RATE_LIMIT_WINDOW = 60 * 60 * 1000; // 1 hora en milisegunndos
+
+function RateLimiting(
+  userId: string,
+  userUploads: Map<string, { count: number; lastReset: number }>,
+  RATE_LIMIT: number,
+  RATE_LIMIT_WINDOW: number
+) {
+  const now = Date.now();
+  const userUploadInfo = userUploads.get(userId) || {
+    count: 0,
+    lastReset: now,
+  };
+  if (now - userUploadInfo.lastReset > RATE_LIMIT_WINDOW) {
+    userUploadInfo.count = 0;
+    userUploadInfo.lastReset = now;
+  }
+  if (userUploadInfo.count >= RATE_LIMIT) {
+    throw new Error("Rate limit exceeded. Please try again later.");
+  }
+  userUploadInfo.count++;
+  console.log(userUploadInfo.count);
+  userUploads.set(userId, userUploadInfo);
 }
 
 export async function actionUploadImage(formData: FormData) {
-
   // Check user authentication
   const { userId } = auth();
   if (!userId) {
@@ -21,10 +48,16 @@ export async function actionUploadImage(formData: FormData) {
 
   // Check if user's email is verified
   const user = await clerkClient.users.getUser(userId);
-  if (!user.emailAddresses.some(email => email.verification?.status === "verified")) {
+  if (
+    !user.emailAddresses.some(
+      (email) => email.verification?.status === "verified"
+    )
+  ) {
     throw new Error("Forbidden: Email not verified");
   }
-  
+
+  RateLimiting(userId, userUploads, RATE_LIMIT, RATE_LIMIT_WINDOW);
+
   const fileField = formData.get("file");
   if (fileField === null) {
     throw new Error(`Missing 'file' field in form`);
@@ -36,7 +69,7 @@ export async function actionUploadImage(formData: FormData) {
 
   // Obtener buffer del fichero
   const file = object as File;
-  const bytes = await file.arrayBuffer()
+  const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
   // Calcula hash del fichero para el nombre
@@ -49,6 +82,6 @@ export async function actionUploadImage(formData: FormData) {
 
   //Enviamos el fichero a r2
   await r2client.uploadFile(uploadFilename, buffer);
-  
+
   return uploadFilename;
 }
